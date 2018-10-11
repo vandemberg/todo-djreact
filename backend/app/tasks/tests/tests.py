73dcc8184dc200db538cf54081c5ac7a621509f5
Vandemberg Lima <vandemberg.silva.lima@gmail.com>
2018-10-10 22:55:35 -0300

    add testing to post request in tasks app

--- backend/app/tasks/tests/tests.py
+++ backend/app/tasks/tests/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from tasks.models import Task
 
-# Create your tests here.
+class TaskTests(APITestCase):
+    def test_create_Task(self):
+        """
+        Ensure we can create a new Task object.
+        """
+        url = '/api/tasks'
+        data = {'title': 'DabApps'}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Task.objects.count(), 1)
+        self.assertEqual(Task.objects.get().title, 'DabApps')

4524cd8b263bc5628f40611504a695b1b088b5d3
Vandemberg Lima <vandemberg.silva.lima@gmail.com>
2018-10-11 02:31:38 -0300

    add a layout with menu and content element

--- frontend/gui/src/App.js
+++ frontend/gui/src/App.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router } from "react-router-dom";
-import Layout from './components/Layout';
+import MainLayout from './components/Layout';
 import BaseRouter from './routes';
+
+// CSS imports
+import 'antd/dist/antd.css';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Router>
-          <Layout {...this.props}>
+          <MainLayout {...this.props}>
             <BaseRouter />
-          </Layout>
+          </MainLayout>
         </Router>
       </div>
     );
--- frontend/gui/src/components/Layout.js
+++ frontend/gui/src/components/Layout.js
@@ -1,12 +1,36 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom'; 
+import { Layout, Menu, Breadcrumb } from 'antd';
+const { Header, Content, Footer } = Layout;
 
-export default class Layout extends React.Component {
+export default class MainLayout extends React.Component {
 
     render() {
         return (
-            <div>
-                My new Layout
-            </div>
+            <Layout className="layout">
+                <Header>
+                <Menu
+                    theme="dark"
+                    mode="horizontal"
+                    defaultSelectedKeys={['2']}
+                    style={{ lineHeight: '64px' }}
+                >
+                    <Menu.Item key="home"> Home </Menu.Item>
+                    <Menu.Item key="report"> Reports </Menu.Item>
+                </Menu>
+                </Header>
+                <Content style={{ padding: '0 50px' }}>
+                <Breadcrumb style={{ margin: '16px 0' }}>
+                    <Breadcrumb.Item> <NavLink to="/">List</NavLink> / </Breadcrumb.Item>
+                </Breadcrumb>
+                <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>
+
+                </div>
+                </Content>
+                <Footer style={{ textAlign: 'center' }}>
+                    LabCodes Challenge
+                </Footer>
+            </Layout>
         )
     }
 

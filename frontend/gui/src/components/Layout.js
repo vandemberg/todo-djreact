08cc78119fffb3d86ca77bad196c586531cc6e3d
Vandemberg Lima <vandemberg.silva.lima@gmail.com>
2018-10-11 01:46:22 -0300

    init layout to react app

---
+++ frontend/gui/src/components/Layout.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Layout extends React.component {
+
+    render() {
+        return (
+            <div>
+                Layout
+            </div>
+        )
+    }
+
+}

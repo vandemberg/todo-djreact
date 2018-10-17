38e5cf14862aafba9968fdf517dc1ae6d75f5c6b
Vandemberg Lima <vandemberg.silva.lima@gmail.com>
2018-10-17 12:43:22 -0300

    card to tasks

--- frontend/gui/src/components/TODOList/TODOList.js
+++ frontend/gui/src/components/TODOList/TODOList.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import { Card, Col, Button, Input  } from 'antd';
+import { Card, Col, Button, Input, Row, Form, DatePicker  } from 'antd';
 import Task from '../Task/Task';
 import TODOListHTTP from '../../http/TODOListHTTP';
+import TaskHttp from '../../http/TaskHTTP';
 
 export default class TODOList extends React.Component {
     
     state = {
-        todoList: {}
+        todoList: {},
+        taskTitle: null,
+        taskDeadline: null
     }
 
     constructor(props){
@@ -32,6 +35,14 @@ export default class TODOList extends React.Component {
         this.setState({todoList});
     }
 
+    onTaskTitle = (e) => {
+        this.setState({taskTitle: e.target.value});
+    }
+
+    onTaskDeadline = (date, dateString) => {
+        this.setState({taskDeadline: dateString});
+    }
+
     delete = (e) => {
         e.preventDefault();
         TODOListHTTP.delete(this.state.todoList.id)
@@ -40,23 +51,78 @@ export default class TODOList extends React.Component {
             });
     }
 
+    listTasks = () => {
+        let tasks = [];
+
+        for(let index in this.state.todoList.tasks) {
+            
+            let my_task = this.state.todoList.tasks[index];
+            my_task.index = index;
+            
+            let taskTag = <Task 
+                            observer={this.observer} 
+                            task={my_task} 
+                            key={index} 
+                            style={{marginRight: '10px'}}/>
+            tasks.push(taskTag);
+        }
+
+        if(tasks.length === 0) {
+            return <center> No one tasks</center>
+        }
+
+        return tasks;
+    }
+
+    createNewTask = (e) => {
+        e.preventDefault();
+        let data = {
+            title: this.state.taskTitle,
+            todo_list: this.state.todoList.id,
+            deadline: this.state.taskDeadline
+        };
+        TaskHttp.create(data)
+            .then((result) => {
+                let task = result.data;
+                let tasks = this.state.todoList.tasks;
+                tasks.push(task);
+                this.setState({tasks});
+            });
+    }
+
+    componentDidUpdate() {
+        this.listTasks();
+    }
+
     render() {
         return (
-            <Col span={6}>
+            <Col span={10}>
                 <Card
                     title={<Input type="text" onChange={this.updateTitle} value={this.state.todoList.title} style={{marginRight: '2px'}}/>}
                     extra={
                         <div>
                             <Button type="dashed" style={{marginRight: '10px'}}
-                                onClick={this.update}
-                            > Editar </Button>
-                            <Button type="danger" onClick={this.delete}>  Remover </Button>
+                                onClick={this.update}> Edit </Button>
+                            <Button type="danger" onClick={this.delete}>  Remove </Button>
 
-                            </div>
+                        </div>
                         }
                     style={{ width: 400, marginBottom: '10px' }}
                 >
-                    <div><Task></Task></div>
+                        <Row>
+                            <Form onSubmit={this.createNewTask}>
+                                <Col span={8} style={{paddingRight: "5px"}}>
+                                        <DatePicker onChange={this.onTaskDeadline}/>
+                                </Col>
+                                <Col span={8} style={{paddingRight: "10px"}}>
+                                    <Input type="text" placeholder="New Task?" onChange={this.onTaskTitle}/>
+                                </Col>
+                                <Col span={8}><Button type="primary" htmlType="submit"> New </Button></Col>
+                            </Form>
+                        </Row>
+                        <Row>
+                            {this.listTasks()}
+                        </Row>                    
                 </Card>
             </Col>
         )
--- frontend/gui/src/components/Task/Task.js
+++ frontend/gui/src/components/Task/Task.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import { Card, Row } from 'antd';
+
+const gridStyle = {
+    width: '100%',
+    textAlign: 'center',
+  };
 
 export default class Task extends React.Component {
+    
+    constructor(props){
+        super(props);
+        this.observer = props.observer;
+    }
+
     render() {
         return (
-            <div>
-                <p> Card Content </p>
-            </div>
-        )
+            <Row style={{marginTop: '10px'}}>
+                <Card.Grid style={gridStyle}>{this.props.task.title}</Card.Grid>
+            </Row>
+        );
     }
 }

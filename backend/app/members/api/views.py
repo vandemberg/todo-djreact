ef9a62ca422a28ba296061f796e202e2259c42b3
Vandemberg Lima <vandemberg.silva.lima@gmail.com>
2018-10-18 00:24:55 -0300

    change the list to find a specific member row in database

--- backend/app/members/api/views.py
+++ backend/app/members/api/views.py
@@ -6,4 +6,12 @@ from members.models import TaskUsers
 
 class MembersViewSet(viewsets.ModelViewSet):
     serializer_class = TaskUsersSerializer
-    queryset = TaskUsers.objects.all()    
+    queryset = TaskUsers.objects.all()
+
+    
+    def list(self, request, format=None):
+        user_id = request.GET["user_id"]
+        task_id = request.GET["task_id"]
+
+        result = TaskUsers.objects.filter(user_id=user_id).filter(task_id=task_id).first()
+        return Response(TaskUsersSerializer(result).data)
--- backend/app/tasks/api/serializers.py
+++ backend/app/tasks/api/serializers.py
@@ -1,15 +1,13 @@
 from rest_framework import serializers
 from tasks.models import Task
 from django.contrib.auth.models import User
+from members.api.serializers import TaskUsersSerializer
+from rest_auth.serializers import UserDetailsSerializer
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('id', 'username')
 
 class TaskSerializer(serializers.ModelSerializer):
 
-    users = UserSerializer(many=True, read_only=True)
+    users = UserDetailsSerializer(read_only=True, many=True)
 
     class Meta:
         model = Task
